3fb7e042a305b4d8c9fcf4a45585322c41262da7
qi1506872929 <1506872929@qq.com>
2021-08-03 18:30:09 +0800

    项目初始化，调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+// @ts-ignore
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
